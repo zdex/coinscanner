141882779f8df670013d03dc52372a7d5a820f4c
gmaheshwari2006 <32501070+gmaheshwari2006@users.noreply.github.com>
2021-01-24 21:45:45 -0500

    update proejct name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RippleScanner'`, () => {
+  it(`should have as title 'CoinScanner'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RippleScanner');
+    expect(app.title).toEqual('CoinScanner');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('RippleScanner app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('CoinScanner app is running!');
   });
 });
